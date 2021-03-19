86d1bd96d11d455058b6a3156f4097c19bc89bbd
reinaldoriant <reinaldoriant@gmail.com>
2021-03-19 19:55:51 +0700

    praktikum1.3

---
+++ src/praktikum_1/praktikum_1_3.java
@@ -0,0 +1,13 @@
+package praktikum_1;
+
+public class praktikum_1_3 {
+    public static void main(String[] args) {
+        int d =130;
+        byte e = (byte)d;
+        System.out.println("hasil ="+e);
+
+        int g =e;
+        System.out.println("hasil ="+g);
+
+    }
+}
